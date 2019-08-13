8eb0e9f750534c01fdea03eb31814619cc4d4144
Marcin <marcin.wojtala@oup.com>
2019-08-13 10:58:26 +0100

    basic post

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const path = require('path')
+const bodyParser = require('body-parser')
 const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
+  .use(bodyParser.urlencoded({ extended: false }))
+  .use(bodyParser.json())
   .get('/', (req, res) => res.render('pages/index'))
+  .post('/', (req, res) => {
+    res.end(true);
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
