3153769598868326ff5c3d691190e669e54f6ca0
Marcin <marcin.wojtala@oup.com>
2019-08-13 11:34:20 +0100

    added basic session

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
+const session = require('express-session')
 const PORT = process.env.PORT || 5000
+
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
+  .use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 }}))
   .use(bodyParser.urlencoded({ extended: false }))
   .use(bodyParser.json())
   .get('/', (req, res) => {
     const passedVariable = req.session.valid;
     console.log('___ SESSION session:valid:: ___ ' + passedVariable);
-    res.render('pages/index')
+    res.render('pages/index', {'student_email':req.session.student_email})
   })
   .post('/', (req, res) => {
-    console.log('=============== REQ ====================')
     console.log(req);
-    console.log('=============== RES ====================')
+    console.log('=============== | REQ ====================')
     console.log(res);
+    console.log('=============== | RES ====================')
     req.session.valid = true;
+    req.session.student_email = res.lis_person_contact_email_primary;
     res.redirect(200, '/survey.html?email='+res.lis_person_contact_email_primary);
     // res.end();
   })
